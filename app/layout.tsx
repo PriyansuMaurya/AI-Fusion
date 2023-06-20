2f458dce655efc3524802d5278195c26dbb37095
Rajkaran <rajkaranvir456@gmail.com>
2023-06-20 16:09:56 +0530

    add: added open graph tags to preview websites and add preview image in public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "AI-Fusion",
-  description: "A website where essential tools, prompts, and datasets are thoughtfully curated and combined in one place for your convenience.",
+  description:
+    "A website where essential tools, prompts, and datasets are thoughtfully curated and combined in one place for your convenience.",
 };
 
 export default function RootLayout({
@@ -19,7 +20,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + "m-10 bg-[--light-bg] dark:bg-[--dark-bg] min-h-screen space-y-auto md:mx-10"}>
+      <head>
+        {/* <!-- Open Graph meta data --> */}
+        <meta property="og:url" content="https://aifusion.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="AI-Fusion - Resources For AI"
+        />
+        <meta
+          property="og:title"
+          content="AI-Fusion"
+        />
+        <meta
+          property="og:description"
+          content="AI Fusion is your ultimate destination for streamlined access to a curated collection of powerful AI tools and prompts. With direct links to a variety of resources, our website eliminates the hassle and empowers you to effortlessly enhance your productivity."
+        />
+        <meta
+          property="og:image"
+          itemProp="image"
+          content="/ai_fusion_preview.png"
+        />
+      </head>
+      <body
+        className={
+          inter.className +
+          "m-10 bg-[--light-bg] dark:bg-[--dark-bg] min-h-screen space-y-auto md:mx-10"
+        }
+      >
         <ThemeContextProvider>
           <Navbar />
           <NavigationLinks />
