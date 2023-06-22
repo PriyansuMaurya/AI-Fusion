9bcd959063c01b0e5751ea88ac0816e4c5f15e41
CYBWEBALI <alimurtuzapatrawala110@gmail.com>
2023-06-22 21:59:31 +0530

    Fix: Footer should remain at bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="m-7 bg-[--light-bg] dark:bg-[--dark-bg] text-center text-[--dark-bg] dark:text-[--light-bg] md:text-xl">
+    <div className="sticky top-[95vh] m-7 bg-[--light-bg] dark:bg-[--dark-bg] text-center text-[--dark-bg] dark:text-[--light-bg] md:text-xl">
       Developed by friends and <a
         className="underline hover:decoration-[--primary-color] decoration-0 underline-offset-2"
         href={"https://github.com/PriyansuMaurya/AI-Fusion/graphs/contributors"}
