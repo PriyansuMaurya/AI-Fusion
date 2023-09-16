c7e8e7de8e96d15af3f802156c40ec2631601e2b
the-shivam-gupta <109647722+the-shivam-gupta@users.noreply.github.com>
2023-09-16 12:02:43 +0530

    Added a title when clicked on the tabs (#54)

--- components/NavigationLinks.tsx
+++ components/NavigationLinks.tsx
@@ -1,13 +1,10 @@
 'use client'
+import React, { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-
-
-
 export default function NavigationLinks() {
-
   const isMobile = useMediaQuery("(max-width: 600px)");
 
   const linkVariant = {
@@ -20,13 +17,25 @@ export default function NavigationLinks() {
     }
   }
 
+  const handleToolsClick = () => {
+    document.title = "AI Fusion - Tools";
+  }
+
+  const handlePromptsClick = () => {
+    document.title = "AI Fusion - Prompts";
+  }
+
+  const handleDatasetsClick = () => {
+    document.title = "AI Fusion - Datasets";
+  }
+
   return (
     <motion.div className='flex text-[--dark-bg] bg-[--light-bg] dark:bg-[--dark-bg] dark:text-[--light-bg] flex-col items-center justify-between ' whileHover='hover' variants={linkVariant}>
-        <div className="w-50 border border-[--dark-bg] dark:border-[--light-bg] rounded-3xl flex flex-row justify-evenly items-center">
-            <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] rounded-l-3xl transition px-4 py-2" href={"/tools"}>Tools</Link>
-            <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] transition px-4 py-2" href={"/prompts"}>Prompts</Link>
-            <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] rounded-r-3xl transition px-4 py-2" href={"/datasets"}>Datasets</Link>
-        </div>
+      <div className="w-50 border border-[--dark-bg] dark:border-[--light-bg] rounded-3xl flex flex-row justify-evenly items-center">
+        <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] rounded-l-3xl transition px-4 py-2" href={"/tools"} onClick={handleToolsClick}>Tools</Link>
+        <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] transition px-4 py-2" href={"/prompts"} onClick={handlePromptsClick}>Prompts</Link>
+        <Link className="w-24 text-center font-semibold hover:bg-[color:var(--primary-color)] hover:text-[--dark-bg] focus:bg-[--primary-color] rounded-r-3xl transition px-4 py-2" href={"/datasets"} onClick={handleDatasetsClick}>Datasets</Link>
+      </div>
     </motion.div>
   )
 }
